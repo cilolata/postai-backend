8d7c8dc11fc0dcdc9929d16bb72f7f61a587a6f6
cilolata <cilolata@gmail.com>
2026-02-19 23:20:08 -0300

    refactor

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -2,10 +2,7 @@ import "dotenv/config";
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage'); 
 
-export const serviceAccount = '/etc/secrets/learned-484621-firebase-adminsdk-fbsvc-bb95384543.json';
-
 export const app = initializeApp({
-  credential: cert(serviceAccount),
   storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET
 });
 
