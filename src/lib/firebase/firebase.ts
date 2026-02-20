b3326b122d453a099394b74afe6ec5114769bbdf
cilolata <cilolata@gmail.com>
2026-02-19 22:31:04 -0300

    refactor

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -2,11 +2,13 @@ import "dotenv/config";
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getStorage } = require("firebase-admin/storage");
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") as String
+
 export const app = initializeApp({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    privateKey: privateKey,
   }),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
