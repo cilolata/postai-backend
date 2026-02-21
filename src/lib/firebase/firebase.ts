cc01fc5be0202bd198ec05eeec084dd550eb28fa
cilolata <cilolata@gmail.com>
2026-02-21 15:05:10 -0300

    change keys

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -2,10 +2,8 @@ import "dotenv/config";
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage'); 
 
-export const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS) : null;
 
 export const app = initializeApp({
-  credential: cert(serviceAccount),
   storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET
 });
 
